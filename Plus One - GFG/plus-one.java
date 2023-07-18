cf23ebeb8a2b09183438aea5c074f3b92c727ec7
sumanth0612 <123049222+sumanth0612@users.noreply.github.com>
2023-07-18 22:17:13 +0530

    Added solution - LeetHub

---
+++ Plus One - GFG/plus-one.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            ArrayList<Integer> arr = new ArrayList<>();
+            
+            for(int i=0 ; i<N ; i++)
+                arr.add(Integer.parseInt(S[i]));
+
+            Solution ob = new Solution();
+            ArrayList<Integer> res = ob.increment(arr,N);
+            
+            for(int i=0; i<res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static ArrayList<Integer> increment(ArrayList<Integer> arr , int N) {
+        // code here
+        int f=1;
+        for(int i=arr.size()-1;i>=0;i--){
+            if(arr.get(i)!=9){
+                arr.set(i,arr.get(i)+1);
+                f=0;
+                break;
+            }
+            else{
+               arr.set(i,0); 
+            }
+        }
+        if(f==1){
+            arr.add(0,1);
+        }
+        return arr;
+    }
+};
